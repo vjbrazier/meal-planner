564e5c22b06a2b602e1a331d2dfc3d50865c60f5
IridescentBlaze <iridescentblazer@gmail.com>
2025-07-21 12:50:35 -0400

    Setup routing and stylesheet link for index page

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 """
 The Server file. Hosts the server.
 """
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    The index page.
+    """
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)
