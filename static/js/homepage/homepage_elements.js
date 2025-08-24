14459c65ba0389010f9d1ac86038a450c2a634b3
IridescentBlaze <iridescentblazer@gmail.com>
2025-08-24 13:38:38 -0400

    Redid how weekdays are laid out Multiple recipes can be added to one day. Deleting a recipe from a day is fully functional as well. Spreading out recipes works much more smoothly now. Error handling is built in, and steps were taken to make features easy to understand.

--- static/js/homepage/homepage_elements.js
+++ static/js/homepage/homepage_elements.js
@@ -8,12 +8,12 @@ const filter_buttons = document.getElementsByClassName('filter-button');
 // Recipe cards
 const data_holders    = document.getElementsByClassName('data-holder');
 const recipe_cards    = document.getElementsByClassName('recipe-card');
+const recipe_links    = document.getElementsByClassName('recipe-link');
 const recipe_names    = document.getElementsByClassName('recipe-name');
 const selected_recipe = document.getElementById('selected-recipe');
 
 // Weekday planning
 const ingredient_list    = document.getElementById('ingredient-list');
-const weekday_buttons    = document.getElementsByClassName('weekday-button');
-const weekday_inner_text = document.getElementsByClassName('weekday-inner-text');
-const weekday_clears     = document.getElementsByClassName('weekday-clear');
-const weekday_multis     = document.getElementsByClassName('weekday-multi');
+const placeholders       = document.getElementsByClassName('weekday-placeholder');
+const planned_meals      = document.getElementsByClassName('planned-meals');
+const add_meals          = document.getElementsByClassName('add-meals');
--- static/js/homepage/weekdays.js
+++ static/js/homepage/weekdays.js
@@ -1,58 +1,193 @@
 // Handles the weekdays functions \\
 // Variables
-let multi_selection = false;
-let recipe_selected = null;
-let weekday_selected = [false, false, false, false, false, false, false]
+let adding_meal = false;
+let recipe_to_spread = '';
+let spreading_meal = false;
+let selected_weekday = null;
 
-// Checks for if cursor is over a weekday button.
-for (let i = 0; i < weekday_buttons.length; i++) {
-    weekday_buttons[i].addEventListener('click', () => {
-        if (multi_selection) {
-            recipe_info = document.getElementById(recipe_selected.toLowerCase());
+function reset_meal_adding() {
+    selected_weekday = null;
+    adding_meal = false;
 
-            weekday_buttons[i].style.backgroundImage = `url(${recipe_info.getAttribute('data-image')})`;
-            weekday_buttons[i].style.backgroundColor = 'var(--button-hover-color)';
-            weekday_inner_text[i].classList.add('visible');
-            weekday_multis[i].classList.add('visible');
-            weekday_inner_text[i].innerText = `${recipe_selected}`;
+    for (let i = 0; i < add_meals.length; i++) {
+        add_meals[i].innerText = '+ Add Meal +';
+    }
+}
+
+for (let i = 0; i < add_meals.length; i++) {    
+    add_meals[i].addEventListener('click', () => {
+        let currently_selected  = add_meals[i].innerText == '+ Add Meal +' ? false : true;
+        
+        if (spreading_meal) {
+            selected_weekday = i;
+            create_planned_meal(i, true);
+        } else {
+            if (!currently_selected) {
+                reset_meal_adding();
+                add_meals[i].innerText = 'Select a meal above!';
+                adding_meal = true;
+                selected_weekday = i;
+            } else {
+                add_meals[i].innerText = '+ Add Meal +';
+                adding_meal = false;
+                selected_weekday = null;
+            }
         }
     })
-
-    weekday_buttons[i].addEventListener(('mouseover'), () => {
-        weekday_selected[i] = true;
-    })
-
-    weekday_buttons[i].addEventListener(('mouseout'), () => {
-        weekday_selected[i] = false;
-    })
 }
 
-// Clears the data inside the weekday
-for (let i = 0; i < weekday_clears.length; i++) {
-    weekday_clears[i].addEventListener('click', () => {
-        weekday_buttons[i].style.backgroundColor = 'var(--button-background-color)';
-        weekday_buttons[i].style.backgroundImage = 'none';
-        weekday_inner_text[i].innerText = '';
-        weekday_inner_text[i].classList.remove('visible');
-        weekday_multis[i].classList.remove('visible');
-    })
+function check_for_planned_meals() {
+    for (let i = 0; i < planned_meals.length; i++) {
+        if (planned_meals[i].children.length == 0) {
+            placeholders[i].classList.add('visible');
+        } else {
+            placeholders[i].classList.remove('visible');
+        }
+    }
 }
 
-// Allows you to make a recipe apply to multiple days without doubling ingredients
-// Allows you to set multiple days for one recipe
-for (let i = 0; i < weekday_multis.length; i++) {
-    weekday_multis[i].addEventListener('click', () => {
-        if (weekday_multis[i].classList.contains('visible')) {
-            for (let j = 0; j < weekday_multis.length; j++) {
-                if (weekday_multis[j].innerText.includes('Disabled')) {
-                    weekday_multis[j].innerText = 'Spread out recipe: Enabled';
+function check_for_spreading(current_button) {
+    let spread_buttons = document.getElementsByClassName('spread-button');
+
+    for (let i = 0; i < spread_buttons.length; i++) {
+        if ((spread_buttons[i].innerText == 'Spreading') && (spread_buttons[i] != current_button)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function create_spread_function(spread_button) {
+    spread_button.addEventListener('click', () => {
+        let recipe_buttons = document.getElementsByClassName('recipe-button');
+        let spread_buttons = document.getElementsByClassName('spread-button');
+        
+        for (let i = 0; i < spread_buttons.length; i++) {
+            if (spread_buttons[i] == spread_button) {
+                let other_recipe_spreading   = check_for_spreading(spread_button);
+                
+                if (!other_recipe_spreading) {
+                    let current_recipe_spreading = spread_buttons[i].innerText == 'Spread' ? false : true;
+                    
+                    if (spread_button.innerText != 'Already Spread') {
+                        if (!current_recipe_spreading) {
+                            reset_meal_adding();
+
+                            for (let k = 0; k < add_meals.length; k++) {
+                                add_meals[k].innerText = 'Spread Recipe Here';
+                            }
+
+                            spread_button.innerText = 'Spreading';
+                            recipe_to_spread = recipe_buttons[i].innerText.toLowerCase().replaceAll(' ', '-');
+                            spreading_meal = true;
+                        } else {
+                            reset_meal_adding();
+                            spread_button.innerText = 'Spread';
+                            recipe_to_spread = '';
+                            spreading_meal = false;
+                        }
+                    }
                 } else {
-                    weekday_multis[j].innerText = 'Spread out recipe: Disabled';
+                    let warning_text = document.getElementById('spread-warning');
+
+                    warning_text.classList.add('visible');
+                    setTimeout(() => {
+                        warning_text.classList.remove('visible');
+                    }, 1000);
                 }
             }
+        }
+    })
+}
 
-            multi_selection = !multi_selection
-            recipe_selected = weekday_inner_text[i].innerText;
+function create_planned_meal(index, spread_meal) {
+    let recipe_button = document.createElement('button');
+    
+    if (spread_meal) {
+        let data_holder = document.getElementById(recipe_to_spread);
+
+        recipe_button.setAttribute('data-measurements', data_holder.getAttribute('data-measurements'));
+        recipe_button.setAttribute('data-ingredients', data_holder.getAttribute('data-ingredients'));
+        recipe_button.innerText = recipe_to_spread.replaceAll('-', ' ').replace(/\b\w/g, char => char.toUpperCase());
+    } else {
+        recipe_button.setAttribute('data-measurements', data_holders[index].getAttribute('data-measurements'));
+        recipe_button.setAttribute('data-ingredients', data_holders[index].getAttribute('data-ingredients'));
+        recipe_button.innerText = recipe_names[index].innerText;
+    }
+    recipe_button.classList.add('recipe-button')
+    
+    let spread_button = document.createElement('button');
+    if (spread_meal) {
+        spread_button.innerText = 'Already Spread';
+    } else {
+        spread_button.innerText = 'Spread';
+    }
+    spread_button.classList.add('spread-button');
+    
+    create_spread_function(spread_button)
+    
+    let clear_button = document.createElement('button');
+    clear_button.innerText = 'X';
+    clear_button.classList.add('clear-button')
+    if (spread_meal) {
+        clear_button.setAttribute('data-spread', 'spread');
+    }
+
+    clear_button.addEventListener('click', () => {
+        let recipe_buttons = Array.from(document.getElementsByClassName('recipe-button'));
+        let spread_buttons = Array.from(document.getElementsByClassName('spread-button'));
+        let clear_buttons  = Array.from(document.getElementsByClassName('clear-button'));
+        
+        for (let j = 0; j < clear_buttons.length; j++) {
+            if (clear_buttons[j] == clear_button) {
+                let outer_spread = clear_buttons[j].getAttribute('data-spread');
+                
+                for (let k = 0; k < recipe_buttons.length; k++) {
+                    let inner_spread = clear_buttons[k].getAttribute('data-spread');
+
+                    if ((inner_spread == 'spread') && (recipe_buttons[j].innerText == recipe_buttons[k].innerText)) {
+                        recipe_buttons[k].remove();
+                        spread_buttons[k].remove();
+                        clear_buttons[k].remove();
+
+                        check_for_planned_meals();
+                    }
+                }
+
+                recipe_buttons[j].remove();
+                spread_buttons[j].remove();
+                clear_buttons[j].remove();
+                
+                check_for_planned_meals()
+
+                if (outer_spread != 'spread') {
+                    console.log('delete');
+                }
+            }
+        }
+    })
+    
+    if (spread_meal) {
+        console.log('spread');
+    } else {
+        console.log('not spread');
+    }
+
+    planned_meals[selected_weekday].appendChild(recipe_button);
+    planned_meals[selected_weekday].appendChild(spread_button);
+    planned_meals[selected_weekday].appendChild(clear_button)
+    check_for_planned_meals();
+
+}
+
+// Appends a recipe to a weekday
+for (let i = 0; i < recipe_links.length; i++) {
+    recipe_links[i].addEventListener('click', (event) => {
+        if (adding_meal) {
+            event.preventDefault();
+
+            create_planned_meal(i, false);
         }
     })
 }
